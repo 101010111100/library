b5a7d8b11ab7e17b92440c851781760bf7f69317
mruz <mruz@poczta.onet.pl>
2012-10-28 20:02:08 +0000

    Valid, isSetOption() instead of getOption()

--- Valid.php
+++ Valid.php
@@ -15,19 +15,19 @@ class Valid extends \Phalcon\Mvc\Model\Validator
                 $filtered = filter_var($value, FILTER_VALIDATE_EMAIL);
                 if ( ! $filtered && $value !== '')
                 {
-                    $this->appendMessage($this->getOption('message') ? $this->getOption('message') : __("Field :field must be a email address", array(':field' => "<em>" . ( $this->getOption('label') ? __($this->getOption('label')) : $field ) . "</em>" )), $field, "email");
+                    $this->appendMessage($this->isSetOption('message') ? $this->getOption('message') : __("Field :field must be a email address", array(':field' => "<em>" . ( $this->isSetOption('label') ? __($this->getOption('label')) : $field ) . "</em>" )), $field, "email");
                     return FALSE;
                 }
             break;
             case 'length':
                 if (strlen($value) < $this->getOption('min'))
                 {
-                    $this->appendMessage($this->getOption('message') ? $this->getOption('message') : __("Field :field must be at least :min characters long", array(':field' => "<em>" . ( $this->getOption('label') ? __($this->getOption('label')) : $field ) . "</em>", ':min' => "<em>" . $this->getOption('min') . "</em>" )), $field, "length");
+                    $this->appendMessage($this->isSetOption('message') ? $this->getOption('message') : __("Field :field must be at least :min characters long", array(':field' => "<em>" . ( $this->isSetOption('label') ? __($this->getOption('label')) : $field ) . "</em>", ':min' => "<em>" . $this->getOption('min') . "</em>" )), $field, "length");
                     return FALSE;
                 }
                 if (strlen($value) > $this->getOption('max'))
                 {
-                    $this->appendMessage($this->getOption('message') ? $this->getOption('message') : __("Field :field must not exceed :max characters long", array(':field' => "<em>" . ( $this->getOption('label') ? __($this->getOption('label')) : $field ) . "</em>", ':max' => "<em>" . $this->getOption('max') . "</em>" )), $field, "length");
+                    $this->appendMessage($this->isSetOption('message') ? $this->getOption('message') : __("Field :field must not exceed :max characters long", array(':field' => "<em>" . ( $this->isSetOption('label') ? __($this->getOption('label')) : $field ) . "</em>", ':max' => "<em>" . $this->getOption('max') . "</em>" )), $field, "length");
                     return FALSE;
                 }
             break;
@@ -35,7 +35,7 @@ class Valid extends \Phalcon\Mvc\Model\Validator
                 $repeat = $this->getOption('match');
                 if ($value !== $model->$repeat)
                 {
-                    $this->appendMessage($this->getOption('message') ? $this->getOption('message') : __("Field :field must be the same as :repeat", array(':field' => "<em>" . ( $this->getOption('label') ? __($this->getOption('label')) : $field ) . "</em>", ':repeat' => "<em>" . ( $this->getOption('match_label') ? __($this->getOption('match_label')) : $repeat ) . "</em>" )), $field, "repeat");
+                    $this->appendMessage($this->isSetOption('message') ? $this->getOption('message') : __("Field :field must be the same as :repeat", array(':field' => "<em>" . ( $this->isSetOption('label') ? __($this->getOption('label')) : $field ) . "</em>", ':repeat' => "<em>" . ( $this->getOption('match_label') ? __($this->getOption('match_label')) : $repeat ) . "</em>" )), $field, "repeat");
                     return FALSE;
                 }
             break;
@@ -43,7 +43,7 @@ class Valid extends \Phalcon\Mvc\Model\Validator
                 $filtered = filter_var($value, FILTER_VALIDATE_URL);
                 if ( ! $filtered && $value !== '')
                 {
-                    $this->appendMessage($this->getOption('message') ? $this->getOption('message') : __("Field :field must be a url", array(':field' => "<em>" . ( $this->getOption('label') ? __($this->getOption('label')) : $field ) . "</em>" )), $field, "url");
+                    $this->appendMessage($this->isSetOption('message') ? $this->getOption('message') : __("Field :field must be a url", array(':field' => "<em>" . ( $this->isSetOption('label') ? __($this->getOption('label')) : $field ) . "</em>" )), $field, "url");
                     return FALSE;
                 }
             break;
