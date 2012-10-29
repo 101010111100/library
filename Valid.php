0628d2c01a4fe491082073c3b7af610460745cce
mruz <mruz@poczta.onet.pl>
2012-10-29 10:13:32 +0000

    Valid, added types: alpha, alpha_dash, alpha_num, digit, ip, range

--- Valid.php
+++ Valid.php
@@ -9,11 +9,51 @@ class Valid extends \Phalcon\Mvc\Model\Validator
 
         switch ($this->getOption('type'))
         {
+            case 'alpha':
+                $filtered = ctype_alpha($value);
+                if ( ! $filtered && $value !== '')
+                {
+                    $this->appendMessage($this->isSetOption('message') ? $this->getOption('message') : __("Field :field must contain only letters", array(':field' => "<em>" . ( $this->isSetOption('label') ? __($this->getOption('label')) : $field ) . "</em>" )), $field, "alpha");
+                    return FALSE;
+                }
+            break;
+            case 'alpha_dash':
+                $filtered = preg_match('/^[-a-z0-9_]++$/iD', $value);;
+                if ( ! $filtered && $value !== '')
+                {
+                    $this->appendMessage($this->isSetOption('message') ? $this->getOption('message') : __("Field :field must contain only numbers, letters and dashes", array(':field' => "<em>" . ( $this->isSetOption('label') ? __($this->getOption('label')) : $field ) . "</em>" )), $field, "alpha_dash");
+                    return FALSE;
+                }
+            break;
+            case 'alpha_num':
+                $filtered = ctype_alnum($value);
+                if ( ! $filtered && $value !== '')
+                {
+                    $this->appendMessage($this->isSetOption('message') ? $this->getOption('message') : __("Field :field must contain only letters and numbers", array(':field' => "<em>" . ( $this->isSetOption('label') ? __($this->getOption('label')) : $field ) . "</em>" )), $field, "alpha_num");
+                    return FALSE;
+                }
+            break;
+            case 'digit':
+                $filtered = ctype_digit($value);
+                if ( ! $filtered && $value !== '')
+                {
+                    $this->appendMessage($this->isSetOption('message') ? $this->getOption('message') : __("Field :field must be a digit", array(':field' => "<em>" . ( $this->isSetOption('label') ? __($this->getOption('label')) : $field ) . "</em>" )), $field, "digit");
+                    return FALSE;
+                }
+            break;
             case 'email':
                 $filtered = filter_var($value, FILTER_VALIDATE_EMAIL);
                 if ( ! $filtered && $value !== '')
                 {
                     $this->appendMessage($this->isSetOption('message') ? $this->getOption('message') : __("Field :field must be a email address", array(':field' => "<em>" . ( $this->isSetOption('label') ? __($this->getOption('label')) : $field ) . "</em>" )), $field, "email");
+                    return FALSE;
+                }
+            break;
+            case 'ip':
+                $filtered = filter_var($value, FILTER_VALIDATE_IP);
+                if ( ! $filtered && $value !== '')
+                {
+                    $this->appendMessage($this->isSetOption('message') ? $this->getOption('message') : __("Field :field must be an ip adderss", array(':field' => "<em>" . ( $this->isSetOption('label') ? __($this->getOption('label')) : $field ) . "</em>" )), $field, "ip");
                     return FALSE;
                 }
             break;
@@ -26,6 +66,13 @@ class Valid extends \Phalcon\Mvc\Model\Validator
                 if (strlen($value) > $this->getOption('max'))
                 {
                     $this->appendMessage($this->isSetOption('message') ? $this->getOption('message') : __("Field :field must not exceed :max characters long", array(':field' => "<em>" . ( $this->isSetOption('label') ? __($this->getOption('label')) : $field ) . "</em>", ':max' => "<em>" . $this->getOption('max') . "</em>" )), $field, "length");
+                    return FALSE;
+                }
+            break;
+            case 'range':
+                if ($value < $this->getOption('min') || $value > $this->getOption('max'))
+                {
+                    $this->appendMessage($this->isSetOption('message') ? $this->getOption('message') : __("Field :field must be within the range of :min to :max", array(':field' => "<em>" . ( $this->isSetOption('label') ? __($this->getOption('label')) : $field ) . "</em>", ':min' => "<em>" . $this->getOption('min') . "</em>", ':max' => "<em>" . $this->getOption('max') . "</em>" )), $field, "range");
                     return FALSE;
                 }
             break;
