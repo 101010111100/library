a6455f64e9cbc85597247284c0407ececbd40583
mruz <mruz@poczta.onet.pl>
2013-01-17 18:54:24 +0000

    Email, config overwriting from config.ini

--- Email.php
+++ Email.php
@@ -20,19 +20,29 @@ class Email extends Email_PHPMailer {
     {
         $email = new Email_PHPMailer();
         
+        if ($_config = \Phalcon\DI::getDefault()->getShared('config')->email)
+            foreach ($_config as $key => $value)
+                $this->_config[$key] = $value;
+        
         foreach (Arr::merge($this->_config, $config) as $key => $value)
             $this->$key = $value;
         
         return $email;
     }
     
-    public function prepare($subject, $to, $view = '', $site = array(), $data = array())
+    public function prepare($subject, $to, $view = '', $data = array())
     {
+        $site = array(
+            'domain' => \Phalcon\DI::getDefault()->getShared('config')->site->domain,
+            'name' => \Phalcon\DI::getDefault()->getShared('config')->site->name,
+            'url' => \Phalcon\DI::getDefault()->getShared('config')->site->url
+        );
+        
         $this->Subject = $subject;
         $this->AddAddress($to);
 
         $template = new \Phalcon\Mvc\View();
-        $template->setViewsDir('../app/views/');
+        $template->setViewsDir(ROOT_PATH . '/app/views/');
         $template->setMainView(NULL);
         
         $template->setVar('site', $site);
